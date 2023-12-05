63a44e8c356bad8d90ad952142df0bb51d01741c
codedvictor <victorkoladeige@gmail.com>
2023-12-05 21:12:47 +0100

    bug --fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,10 @@
 export default function cleanSet(set, startString) {
+  if (startString.length === 0 || startString === undefined)
+    return '';
+
   const valueFilter = Array.from(set)
-    .filter((valueset) => valueset.startsWith(startString))
-    .map((valueset) => valueset.substring(startString.length))
+    .filter((valueset) => (valueset === undefined ? '' : valueset.startsWith(startString)))
+    .map((valueset) => (valueset === undefined ? '' : valueset.replace(startString, '')))
     .join('-');
 
   return valueFilter;
