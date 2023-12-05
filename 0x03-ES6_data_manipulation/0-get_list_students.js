f5ea82cff18be4d1fcb0f73b6337c5d492b4bf23
codedvictor <victorkoladeige@gmail.com>
2023-12-05 09:04:06 +0100

    bug fixing

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
-export function getListStudents () {
-  studentArray = [
-    {id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    {id: 2, firstName: 'James', location: 'Columbia' },
-    {id: 3, firstName: 'Serena', location: 'San Francisco' },
+export default function getListStudents() {
+  const studentArray = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 3, firstName: 'Serena', location: 'San Francisco' },
   ];
 
   return studentArray;
