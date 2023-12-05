29af4a3c51f316abe2910390882acd892e8906f0
codedvictor <victorkoladeige@gmail.com>
2023-12-05 22:21:16 +0100

    fix bug 3

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,9 @@
 export default function cleanSet(set, startString) {
-  let valueFilter = '';
-  if (startString.length === 0 || startString === undefined) return '';
-  valueFilter = [...set]
+  let nString = '';
+  if (startString === undefined || startString.length === 0) return '';
+  nString = [...set]
     .filter((vls) => (vls === undefined ? '' : vls.startsWith(startString)))
     .map((vls) => (vls === undefined ? '' : vls.replace(startString, '')))
     .join('-');
-
-  return valueFilter;
+  return nString;
 }
