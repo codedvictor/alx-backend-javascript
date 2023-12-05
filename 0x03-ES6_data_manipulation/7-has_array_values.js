1a3cd5ef05a109b56adf94059627450a520fe97a
codedvictor <victorkoladeige@gmail.com>
2023-12-05 16:08:03 +0100

    bug fix

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,5 +1,5 @@
 export default function hasValuesFromArray(set, array) {
-  const valueset = array.every((setvalue) => set.has(value));
+  const valueset = array.every((setvalue) => set.has(setvalue));
 
   return valueset;
 }
